29d93bdb08a540c7f4223420fb09fe87db52f8d9
robernom <rober95@hotmail.com>
2016-10-10 09:42:15 +0200

    Con fichero JSON

--- karaoke.py
+++ karaoke.py
@@ -2,21 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-
-def str_list(list):
-    strExit = "\n"
-    for element in datos:
-        for tag in element:
-            strExit += tag
-            for atr in element[tag]:
-                for val in atr:
-                    strExit += "\t" + val + "=" + '"' + atr[val] + '"'
-        strExit += "\n"
-    return strExit
 
 if __name__ == "__main__":
     parser = make_parser()
@@ -26,5 +16,16 @@ if __name__ == "__main__":
         parser.parse(open(sys.argv[1]))
     except:
         sys.exit("Usage: Python karaoke.py file.smil")
-    datos = prueba.get_tags()
-    print(str_list(datos))
+    lista = prueba.get_tags()
+    strExit = "\n"
+    for element in lista:
+        for tag in element:
+            strExit += tag
+            for atr in element[tag]:
+                for val in atr:
+                    strExit += "\t" + val + "=" + "\"" + atr[val] + "\""
+        strExit += "\n"
+    print(strExit)
+    with open(sys.argv[1][:-5] + ".json", 'w') as doc_json:
+        lista_json = json.dumps(lista)
+        json.dump(lista_json, doc_json)
